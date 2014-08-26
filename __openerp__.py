42de784e989af07dcc161bd05a26c52d38277dc5
accioma <java.diablo@gmail.com>
2014-08-26 05:23:21 -0500

    [ADD] Tour / Manager security

--- __openerp__.py
+++ __openerp__.py
@@ -2,21 +2,23 @@
 {
     "name":"Tour Operation",
     "description":"""\
-Tour Operator functionality
+Tour Operation functionality
 _____________________________________________
 
 - Manage tour products:
-  Cruises, acommodations, flight tickets, lodges, transfers, miscellaneous. TODO:
+  Cruises, accommodations, flight tickets, lodges, transfers, miscellaneous.
 - Travel itinerary. TODO:
 - Manage pax (passengers). TODO:
 - Invoice creation. TODO:
 - Account invoice integration. TODO:
     """,
-    "category":"Tour & Travel",
+    "category":"Tour & Travel Operation",
     "author":"Accioma",
-    "data":[  "views/tourproduct_view.xml"
-            , "views/tourcategories_view.xml"
-            , "views/product_product_view.xml"
+    "data":["security/res_groups.xml",
+            "security/ir.model.access.csv",
+            "views/tourproduct_view.xml",
+            "views/tourcategories_view.xml",
+            "views/product_product_view.xml",
            ],
 
     "depends":["base", "sale", "crm"],
